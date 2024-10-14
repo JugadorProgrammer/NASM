44eac59064d43aae25b13c0bea308d3e1672bdfc
Ilya <JugadorProgrammer>
2024-10-14 17:19:13 +0300

    Первая рабочая реализация 4 лабы

--- LaboratiryWork4/laba4.asm
+++ LaboratiryWork4/laba4.asm
@@ -1,115 +1,101 @@
-global _start           ; делаем метку метку _start видимой извне
+global _start
 
 section .data
-a: dq -6
-b: dq 4
-c: dq 8
-d: dq 2
-x: dq -2
-x_delta: dq 1
-x_destination: dq 5
+first_array dw 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10
+second_array dw  20, 20, 20, 20, 20, 20, 20, 20, 20, 20
+result_array  dw 10 dup(0)
+
+array_lenght: db 10
 result_sign: db 0
-message db "f(x) =          ", 0xA
-message_legth: equ $-message      ; length of msg
+array_number_position: db 7
+
+message db "array -           ", 0xA
+message_length: equ $-message      ; length of msg
 
 section .text           ; code section
 _start:                 ; enter point
+    mov edx, 0
+    movzx ecx, word [array_lenght]
 
-    mov rax, [x]
-    cmp rax, 0
+    sub_loop:
+        mov esi, first_array
+        movzx eax, word [esi + edx * 2] ; first_array element
 
-    jl .y1 ; if x < 0
-    jg .y3 ; if x > 0
-    jmp .y2 ; if x == 0
+        mov esi, second_array
+        movzx edi, word [esi + ecx * 2 - 2]
 
-    .y1:
-        ;2·a·x+5 start
-        mov rax, [a]
-        mov rdi, 2
-        imul rdi
+        sub edi, eax
+        mov esi, result_array
+        mov [esi + edx * 2], edi
 
-        mov rdi, [x]
-        imul rdi
-        add rax, 5
-        ;2·a·x+5 end
+        movzx eax, word [array_lenght]
+        add edx, 1
+    loop sub_loop
 
-        jmp .print_result
-    .y2:
-        ;(a-b)/d start
-        mov rax, [a]
-        sub rax, [b]
+    mov r9d, first_array
+    ;call print_all
 
-        cqo ; для знакового деления
-        mov rdi, [d]
-        idiv rdi
-        ;(a-b)/d end
-        jmp .print_result
+    mov r9d, second_array
+    ;call print_all
 
-    .y3:
-        ;(a^2-x)/(c+d) start
-        mov rax, [a]
-        imul rax
-        sub rax, [x]
+    mov r9d, result_array
+    call print_all
+    
+    ; Завершение программы
+    mov eax, 60 ; sys_exit
+    mov edi, 0 ; код завершения 0
+    syscall
 
-        mov rdi, [c]
-        add rdi, [d]
+print_all:
+    ; movzx ecx, word [array_lenght]
+    mov r8, 0
+    mov rcx, 10
 
-        cqo ;для знакового деления
-        idiv rdi
-        ;(a^2-x)/(c+d) end
-    .print_result:
-        ; start print
-        test rax, rax
-        jns .output
+    print_loop:
+        movzx rax, word [r9d + r8d * 2]
+        call print
 
-        mov dl, 1
+        mov dl, 0
         mov [result_sign], dl
-        neg rax
+        add r8d, 1
+    loop print_loop
+    ret
 
-        ;Поготовка к выводу
-        .output:
+print:
+    mov r10 ,rcx
+    test rax, rax
+    jns .output
+
+    mov dl, 1
+    mov [result_sign], dl
+    neg rax
+
+    .output:
         mov rsi, message
-        add rsi, message_legth-1
+        add rsi, message_length - 2
         mov rbx, 10
         .next_digit:
             xor rdx, rdx       ; clear rdx prior to dividing rdx:rax by rbx
             div rbx            ; rax /= 10
             add dl, '0'        ; convert the remainder to ASCII 
             dec rsi            ; store characters in reverse order
-            mov [rsi], dl      ;
+            mov [rsi], dl
             test rax, rax
-            jnz .next_digit    ; repeat until
-    
+        jnz .next_digit    ; repeat until
+
         mov dl, 1
         cmp [result_sign], dl
-        jnz .print
+        jnz .write
 
         dec rsi
-        mov dl, 45 ; 45 is minus
+        mov dl, '-' ; minus
         mov [rsi], dl
-    
-        .print:
-        mov rdx, message_legth      ; length of the string
-        mov rsi, message         ; address of the string
-        mov rdi, 1           ; file descriptor, in this case stdout
-        mov rax, 1           ; syscall number for write
-        syscall
-        ; end print
-
-    mov rax, [x]
-    mov rdi, [x_destination]
-    cmp rax, rdi
-    jz .exit ; if x == x_destination
-
-    ; x+=delta_x start
-    mov rax, [x_delta]
-    add rax, [x]
-    mov [x], rax
-    ; x+=delta_x end
-
-    jmp _start
-    .exit:
-        ; Завершение программы
-        mov rax, 60 ; sys_exit
-        mov rdi, 0 ; код завершения 0
-        syscall
+        
+        .write:
+            mov rdx, message_length      ; length of the string
+            mov rsi, message         ; address of the string
+            mov rdi, 1           ; file descriptor, in this case stdout
+            mov rax, 1           ; syscall number for write
+            syscall
+    mov rcx, r10
+    ret
