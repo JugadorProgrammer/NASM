40ed346c19223c8eeffe0195faca5092751587b5
Ilya <JugadorProgrammer>
2024-10-26 19:31:18 +0300

    Переделана 4 лабораторная

--- LaboratiryWork3/laba3.asm
+++ LaboratiryWork3/laba3.asm
@@ -1,13 +1,12 @@
 global _start ; _start
 
 section .data
-a: dq 29
-b: dq 18
-c: dq 31
-d: dq -14
+a: dq 29 ;11101
+b: dq 18 ;10010
+c: dq 31 ;11111
+d: dq -14 ;-1110
 
 ; (!b + c * a) + d
-
 result_sign: db 0
 message db "result =          ", 0xA
 message_legth: equ $-message      ; length of msg
@@ -28,10 +27,11 @@ _start:                 ; enter point
 
     ; (!b + c * a) + d start
     mov rdi, [d]
-    and rax, rdi
+    or rax, rdi
     ; (!b + c * a) + d end
     
     ; start print
+    .prt:
     test rax, rax
     jns .output
 
@@ -42,24 +42,24 @@ _start:                 ; enter point
     .output:
         mov rsi, message
         add rsi, message_legth-1
-        mov rbx, 2;10
+        mov rbx, 2
         .next_digit:
             xor rdx, rdx       ; clear rdx prior to dividing rdx:rax by rbx
-            div rbx            ; rax /= 10
+            div rbx            ; rax /= 2
             add dl, '0'        ; convert the remainder to ASCII 
             dec rsi            ; store characters in reverse order
             mov [rsi], dl      ;
             test rax, rax
             jnz .next_digit    ; repeat until
-    
+
         mov dl, 1
         cmp [result_sign], dl
         jnz .print
 
         dec rsi
-        mov dl, 45 ; 45 is minus
+        mov dl, '-' ;
         mov [rsi], dl
-    
+
         .print:
         mov rdx, message_legth      ; length of the string
         mov rsi, message         ; address of the string
--- LaboratiryWork4/laba4.asm
+++ LaboratiryWork4/laba4.asm
@@ -1,44 +1,57 @@
 global _start
 
 section .data
-first_array dw 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10
+first_array dw 40, 39, 38, 37, 36, 5, 34, 32, 31, 30
 second_array dw  20, 20, 20, 20, 20, 20, 20, 20, 20, 20
 result_array  dw 10 dup(0)
 
 array_lenght: db 10
 result_sign: db 0
-array_number_position: db 7
 
-message db "array -           ", 0xA
-message_length: equ $-message      ; length of msg
+message1 db "first  -            ;", 0xA
+message_length: equ $-message1      ; length of msg
+free_space_in_message: equ 10
+
+message2 db "second -            ;", 0xA
+message3 db "result -            ;", 0xA
+
+void_row db "", 0xA
 
 section .text           ; code section
 _start:                 ; enter point
     mov edx, 0
-    movzx ecx, word [array_lenght]
+    mov r8b, [array_lenght] ; save length
+    movsx ecx, byte r8b
 
     sub_loop:
         mov esi, first_array
-        movzx eax, word [esi + edx * 2] ; first_array element
+        movsx eax, word [esi + edx * 2] ; first_array element
 
         mov esi, second_array
-        movzx edi, word [esi + ecx * 2 - 2]
+        movsx edi, word [esi + ecx * 2 - 2]
 
         sub edi, eax
         mov esi, result_array
         mov [esi + edx * 2], edi
 
-        movzx eax, word [array_lenght]
+        movsx eax, byte [array_lenght]
         add edx, 1
     loop sub_loop
 
+    mov [array_lenght], r8b ; restore length
+
     mov r9d, first_array
-    ;call print_all
+    mov esi, message1
+    call print_all
 
+    call print_void_row
     mov r9d, second_array
-    ;call print_all
+    mov rsi, message2
+    call print_all
 
+    call print_void_row
     mov r9d, result_array
+    mov rsi, message3
     call print_all
     
     ; Завершение программы
@@ -47,55 +60,79 @@ _start:                 ; enter point
     syscall
 
 print_all:
-    ; movzx ecx, word [array_lenght]
-    mov r8, 0
-    mov rcx, 10
+    mov r8d, 0
+    movzx ecx, byte [array_lenght]
 
     print_loop:
-        movzx rax, word [r9d + r8d * 2]
+        movsx eax, word [r9d + r8d * 2]
+        
+        mov r10d ,ecx ; save ecx
         call print
+        mov ecx, r10d ; restore ecx
 
         mov dl, 0
         mov [result_sign], dl
-        add r8d, 1
+        inc r8d
+
     loop print_loop
     ret
 
 print:
-    mov r10 ,rcx
-    test rax, rax
+    test eax, eax
     jns .output
 
     mov dl, 1
     mov [result_sign], dl
-    neg rax
+    neg eax
 
     .output:
-        mov rsi, message
-        add rsi, message_length - 2
-        mov rbx, 10
+        mov r11d, message_length - 2
+        add esi, message_length - 2
+
+        mov ebx, 10
         .next_digit:
-            xor rdx, rdx       ; clear rdx prior to dividing rdx:rax by rbx
-            div rbx            ; rax /= 10
+            xor edx, edx       ; clear rdx prior to dividing edx:eax by ebx
+            div ebx            ; eax /= 10
             add dl, '0'        ; convert the remainder to ASCII 
-            dec rsi            ; store characters in reverse order
-            mov [rsi], dl
-            test rax, rax
+            dec esi            ; store characters in reverse order
+            mov [esi], dl
+            dec r11d
+            test eax, eax
         jnz .next_digit    ; repeat until
 
         mov dl, 1
         cmp [result_sign], dl
         jnz .write
 
-        dec rsi
+        dec esi
+        dec r11d
         mov dl, '-' ; minus
-        mov [rsi], dl
+        mov [esi], dl
         
         .write:
-            mov rdx, message_length      ; length of the string
-            mov rsi, message         ; address of the string
-            mov rdi, 1           ; file descriptor, in this case stdout
-            mov rax, 1           ; syscall number for write
+            mov edx, message_length      ; length of the string
+            sub esi, r11d         ; restore address of the string
+            mov edi, 1           ; file descriptor, in this case stdout
+            mov eax, 1           ; syscall number for write
             syscall
-    mov rcx, r10
+        ;call clearr11d_message
+    ret
+
+clear_message:
+    mov ecx, free_space_in_message
+    add esi, message_length - 2 
+    xor edx, edx
+    mov dl, ' '
+    clear_loop:
+        mov [esi], dl
+        dec esi
+        loop clear_loop
+    ret
+
+print_void_row:
+    mov edx, 1      ; length of the string
+    mov esi, void_row
+    mov edi, 1           ; file descriptor, in this case stdout
+    mov eax, 1           ; syscall number for write
+    syscall
     ret
